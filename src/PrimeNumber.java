c8bc3ca479e9f1c401c283600bc76b550e8f3de2
masudulhaque97 <masud5912@gmail.com>
2023-05-01 19:44:17 -0400

    Added missing files

---
+++ src/PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int n;
+		int status = 1;
+		int num = 1;
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number of prime numbers");
+		 
+		n = scanner.nextInt();
+
+		for (int i = 2; i <= 10;) {
+			for (int j = 2; j <= Math.sqrt(num); j++) {
+				if (num % j == 0) {
+					status = 0;
+					break;
+				}
+			}
+			if (status != 0) {
+				System.out.println(num);
+				i++;
+			}
+			status = 1;
+			num++;
+		}
+	}
+}
